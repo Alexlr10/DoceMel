bbe89fe85ed3d71e4a98f10b37be53cab4583806
Alex Lopes <alexlr10@hotmail.com>
2020-03-30 12:50:31 -0300

    dlc

--- legacy/settings.py
+++ legacy/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = ['https://dlcpolpas.herokuapp.com']
+ALLOWED_HOSTS = ['dlcpolpas.herokuapp.com']
 
 LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/'
